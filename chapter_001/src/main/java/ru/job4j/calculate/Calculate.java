61a82b3b21d3afab24acd606efe8a65b5b9b6fe4
SirtsevSergey <artman99@yandex.ru>
2018-12-10 01:50:37 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,19 @@
 package ru.job4j.calculate;
 
+/**
+*Конструктор вывод строки в консоль.
+*@author sergey surtsev (artman99@yandex.ru)
+*@since 09/12/2018
+*version 1
+*/
 public class Calculate {
-	
+	/**
+	*Main.
+	*param args - args
+	*/	
 	public static void main (String [] args) 
 	{
-		System.out.println("We will not use 'Hello, World!");
+		System.out.println("I will not use 'Hello, World!");
 	}
 	
                        }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Sergey Surtsev artman99@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
