799a8c1089b508ba0b724864f866399013af4fb7
SirtsevSergey <artman99@yandex.ru>
2019-01-04 01:27:46 +0300

    3.3. Идеалный вес

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,50 +1,23 @@
 package ru.job4j.calculate;
 
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
 
-/**
- * Test
- * @author Sergey Surtsev (artman99@yandex.ru)
- * @version 1
- * @since 27.12.2018
- */
-public class CalculateTest  {
-    @Test
-    public void whenAddOnePlusOneThenTwo()  {
-        Calculate calc = new Calculate();
-        calc.add(1D, 1D);
-        double result = calc.getResult();
-        double expected = 2D;
-        assertThat(result, is(expected));
+public class CalculateTest {
 
+    @Test
+    public void manWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, closeTo(92.0, 0.1));
     }
-    @Test
-    public void whenSubstructThreeMinusOneThenTwo()  {
-        Calculate calc = new Calculate();
-        calc.substruct(3D, 1D);
-        double result = calc.getResult();
-        double expected = 2D;
-        assertThat(result, is(expected));
 
-    }
     @Test
-    public void whenDivFourOnOneThenFour()  {
-        Calculate calc = new Calculate();
-        calc.div(4D, 1D);
-        double result = calc.getResult();
-        double expected = 4D;
-        assertThat(result, is(expected));
-
-    }
-    @Test
-    public void whenMultipleThreeOnTwoThenSix()  {
-        Calculate calc = new Calculate();
-        calc.multiple(3D, 2D);
-        double result = calc.getResult();
-        double expected = 6D;
-        assertThat(result, is(expected));
-
+    public void womanWeight() {
+        Fit fit = new Fit();
+        double weight = fit.womanWeight(170);
+        assertThat(weight, closeTo(69.0, 0.1));
     }
 }
