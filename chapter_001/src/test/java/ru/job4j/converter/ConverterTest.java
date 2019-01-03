1c92747293e9a2c8f16f91fe46a2efc5e28c5f75
SirtsevSergey <artman99@yandex.ru>
2019-01-03 23:00:41 +0300

    3.2. Конвертер валюты

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Sergey Surtsev (artman99@yandex.ru)
+ * @version 1
+ * @since 27.12.2018
+ */
+
+public class ConverterTest {
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+}
