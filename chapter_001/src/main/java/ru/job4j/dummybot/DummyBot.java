53583c2f2f25f44641fcacd76db4fa5625dfa2a6
SirtsevSergey <artman99@yandex.ru>
2019-04-26 22:38:38 +0300

    4.1 Глупый бот

---
+++ chapter_001/src/main/java/ru/job4j/dummybot/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.dummybot;
+
+/**
+ * Вычисляем растояние между точками в системе кординат
+ *
+ * @author Sergey Surtsev (artman99@yandex.ru)
+ * @version 1
+ * @since 29.12.2018
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String result = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            result = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            result = "До скорой встречи.";
+        }
+        return result;
+    }
+}
